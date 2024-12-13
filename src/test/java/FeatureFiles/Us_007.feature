f2635d18752f86c1f025fb520d0cbff78d2a6882
Gold-S <utkudemir24@gmail.com>
2024-12-13 13:54:56 +0300

    Hazır

---
+++ src/test/java/FeatureFiles/Us_007.feature
@@ -0,0 +1,37 @@
+Feature: Hamburger Menu Messaging Functionality
+
+  Background:
+    Given Navigate to Mersys
+    When Enter username and password and click login button
+    Then User should login successfully
+
+  Scenario: Manage messages.
+
+    Given The Write Message option should be clicked on the Hamburger Menu.
+    When Write new message links must be seen and clicked
+    Then Clicked Messages
+
+  Scenario: Share the message.
+
+    Given Click Message from the hamburger menu
+    When Student chooses the people to send messages to and sends the message
+    Then Sees sent messages in the list
+
+  Scenario: Edit messages.
+
+    Given Hamburger clicks on the message button from the menu and clicks on the outbox box
+    When You should see a confirmation button to delete the message
+    Then Message deleted successfully
+
+
+    Scenario: Recovering and permanently deleting deleted messages.
+
+      Given Click on the messaging trash box from the hamburger menu
+      When You should see a delete and undo icon
+      Then  Messages deleted successfully
+
+
+
+
+
+
--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -1,12 +1,46 @@
 package Pages;
 
 import Utilities.GWD;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class DialogContent extends ParentPage{
+public class DialogContent extends ParentPage {
     public DialogContent() {
         PageFactory.initElements(GWD.getDriver(), this);
     }
+
+    //US_007
+    @FindBy(xpath = "(//div[@class='ng-star-inserted'])//span[@class='mat-ripple mat-mdc-button-ripple']")
+    public WebElement menu;
+
+    @FindBy(xpath = "(//span[@class='mdc-button__label'])[7]")
+    public WebElement more;
+
+    @FindBy(xpath = "(//span[@class='mat-mdc-menu-item-text'])[1]")
+    public WebElement messaging;
+
+    @FindBy(xpath = "(//span[@class='mat-mdc-menu-item-text'])[10]")
+    public WebElement trash;
+
+    @FindBy(xpath = "//span[@style='color: red;']")
+    public WebElement çopKutusu;
+
+    @FindBy(xpath = "(//span[@class='mat-mdc-menu-item-text'])[3]")
+    public WebElement restore;
+
+    @FindBy(xpath = "//div[@style='font-size: 15px;']")
+    public WebElement confirmMesajı;
+
+    @FindBy(xpath = "//span[@class='mat-content mat-content-hide-toggle']")
+    public WebElement successMesajı;
+
+
+
+
+
 
 
 
